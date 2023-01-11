7f608da339b20eac605148b5529776a02dd69de4
zhanggianluca <99273189+zhanggianluca@users.noreply.github.com>
2023-01-11 13:32:40 -0500

    Create se.js

---
+++ se.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const server = http.createServer((req, res)=> {
+    if (req.url === '/') {
+        res.write("HELLO WORLD"); 
+        res.end(); 
+    }
+    //second route
+    if (req.url === "/api/courses") {
+        res.write(JSON.stringify([1,2,3]));
+        res.write("This is a list of offerings at BTHS"); 
+        res.end(); 
+    }
+});
+
+server.listen(3000); //localhost:3000
+console.log("Listening on port 3000 ..."); 
+
+//npx nodemon se.js
+//nodemon se.js
+//npm install -g nodemon
