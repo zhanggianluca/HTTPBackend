5afd5ab8383d05fd4ba2d352b5bf5242a127cac1
zhanggianluca <99273189+zhanggianluca@users.noreply.github.com>
2023-01-23 13:58:04 -0500

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,4 +1,5 @@
 const express = require("express"); 
+const { read } = require("fs");
 const app = express(); 
 app.use(express.json()); 
 
@@ -14,6 +15,15 @@ const courses = [
 
 app.get('/api/courses', (req,res)=> {
      res.send(courses); 
+})
+
+app.get('/api/courses/:id', (req,res)=> {
+    const course = courses.find(c=> c.id === parseInt(req.params.id)); 
+    if(!course) {
+        req.status(404).send("The course with the given ID was not found"); 
+        return
+    }
+        res.send(course); 
 })
 
 //HTTP POST REQUESTS
@@ -34,12 +44,29 @@ app.post('/api/courses', (req,res) => {
 
 //PUT REQUESTS
 app.put("/api/courses/:id", (req, res) => {
-    const course = {
-        id: req.body.id,
-        name: req.body.name
+    const course = courses.find(c=> c.id === parseInt(req.params.id)); 
+    if(!course) {
+        req.status(404).send("The course with the given ID was not found"); 
+        return
     }
+    course.name = req.body.name; 
+    course.id = req.body.id; 
+    courses.splice(course.id-1, 1, course)
+    res.send(course); 
+})
+
+//DELETE REQUESTS
+app.delete("/api/courses/:id", (req, res) => {
+    const course = courses.find(c=> c.id === parseInt(req.params.id)); 
+    if(!course) {
+        req.status(404).send("The course with the given ID was not found"); 
+        return
+    }
+    courses.splice(courses.indexOf(course), 1)
+    res.send(course); 
 })
 
 app.listen(3000, () => {
     console.log("Listening on port 3000 ..."); 
 }); 
+
